831a493cf708077ce4dcd9a34b5b97b344bda682
Volodymyr Udsianskiy <vv7827242@gmail.com>
2024-10-29 13:44:06 +0200

    add

---
+++ js/main.js
@@ -0,0 +1,54 @@
+let celsi = 20;
+
+let farengeit = (celsi * 9 / 5) + 32;
+
+console.log("Температура за Цельсієм " + celsi);
+console.log("Температура за Фаренгейтом " + farengeit);
+
+let days = 30;
+let hours = (days * 24);
+let minuts = (hours * 60);
+
+console.log("Днів в місяці " + days);
+console.log("Годин в місяці " + hours);
+console.log("Хвилин в місяці " + minuts);
+
+let life = 472;
+let mana = 303;
+console.log("Хп = " + life);
+console.log("Мана = " + mana);
+
+life = (472 - 129);
+mana = (303 - 200);
+console.log("Після удару Хп = " + life);
+console.log("Після удару Мана = " + mana);
+
+let sum = 1250;
+let discount = sum * 0.9;
+
+console.log("Сума " + sum);
+console.log("Зі Знижкою " + discount);
+
+let numberFlout = 32.17;
+
+console.log(Math.floor(numberFlout));
+
+let lineFlout = "124.56jhhjkmnb9"
+
+console.log(Number.parseFloat(lineFlout));
+
+
+let line = "74jhhjbvgjh765kmbfbc06vnnb9"
+
+console.log(Number.parseInt(line));
+
+let number = 202;
+
+console.log(Math.sqrt(number));
+
+let wholeNumber = 42;
+
+let lineNumber = "163";
+
+console.log(wholeNumber.toString());
+console.log(Number.parseInt(lineNumber));
